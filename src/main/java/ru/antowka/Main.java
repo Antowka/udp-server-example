f0fd4b6ef4fcf0ff86f5d018bf2f7b01b0f1627b
Anton Nikanorov <662307@gmail.com>
2017-04-04 18:05:29 +0300

    update for udp-tcp-aprs proxy

--- src/main/java/ru/antowka/Main.java
+++ src/main/java/ru/antowka/Main.java
@@ -1,29 +1,16 @@
 package ru.antowka;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import ru.antowka.server.UDPServer;
 
 public class Main {
 
     public static void main(String args[]) throws Exception {
 
-        DatagramSocket serverSocket = new DatagramSocket(7779);
-
-        byte[] receiveData = new byte[1024];
-
-        while(true) {
-
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port = receivePacket.getPort();
-            String capitalizedSentence = sentence.toUpperCase();
-            byte[] sendData = capitalizedSentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            serverSocket.send(sendPacket);
+        int port = 7779;
+        if (args.length != 0) {
+            port = Integer.valueOf(args[0]);
         }
+
+        UDPServer.startUdpServer(port);
     }
 }
--- src/test/ru/antowka/MainTest.java
+++ src/test/ru/antowka/MainTest.java
@@ -2,16 +2,11 @@ package ru.antowka;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by anton on 4/4/17.
@@ -21,15 +16,15 @@ class MainTest {
     @Test
     void testUdpServer() throws Exception {
 
-        new Thread(() -> {
-            try {
-                Main.main(new String[]{});
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-
-        Thread.sleep(1500);
+//        new Thread(() -> {
+//            try {
+//                Main.main(new String[]{"7779"});
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+//
+//        Thread.sleep(1500);
 
         demoClientUDP();
     }
@@ -38,14 +33,14 @@ class MainTest {
 
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = "Test packet data".getBytes(StandardCharsets.UTF_8);
+        byte[] sendData = "UC6KFQ>APRS,TCPIP*,qAC,T2NL:!5619.08N/04403.22Eyop.Anton (UC6KFQ/3) 145.500Mhz/433.500Mhz\n".getBytes(StandardCharsets.UTF_8);
         byte[] receiveData = new byte[1024];
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7779);
         clientSocket.send(sendPacket);
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence);
+//        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//        clientSocket.receive(receivePacket);
+//        String modifiedSentence = new String(receivePacket.getData());
+//        System.out.println("FROM SERVER:" + modifiedSentence);
         clientSocket.close();
     }
 }
